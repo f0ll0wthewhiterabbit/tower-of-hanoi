c4ae733c088a5b7fd6d4f3cae0ac0973d612a01e
f0ll0wthewhiterabbit <3tonishere@gmail.com>
2024-10-27 23:27:29 +0300

    Setup gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/tower-of-hanoi/',
   plugins: [react(), tsconfigPaths()],
   resolve: { alias: { '@': path.resolve(__dirname, './src') } },
 })
